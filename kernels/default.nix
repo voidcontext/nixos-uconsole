1da00f40c1b14d3c27b04084b0d52a81ac5f83d1
Gabor Pihaj <gabor.pihaj@gmail.com>
2024-04-05 00:02:35 +0100

    Add 'offical' kernel patches + tweaked ones, none of the kernels compile

--- kernels/default.nix
+++ kernels/default.nix
@@ -11,4 +11,20 @@
     inherit nixpkgs lib nixos-hardware;
     cross = true;
   };
+  "5.10-clockwork" = import ./5.10-clockwork.nix {
+    inherit nixpkgs lib;
+    cross = false;
+  };
+  "5.10-clockwork-cross-build" = import ./5.10-clockwork.nix {
+    inherit nixpkgs lib;
+    cross = true;
+  };
+  "5.10-jh-110" = import ./5.10-jh-100.nix {
+    inherit nixpkgs lib;
+    cross = false;
+  };
+  "5.10-jh-110-cross-build" = import ./5.10-jh-100.nix {
+    inherit nixpkgs lib;
+    cross = true;
+  };
 }
